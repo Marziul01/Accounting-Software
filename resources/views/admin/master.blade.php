6d67ee7e6291e41277da537711858d5a87fa2ae6
Marziul <marziulhaque08@gmail.com>
2025-05-11 03:59:50 +0600

    toaster

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -141,6 +141,23 @@
         });
       </script>
 
+      <script>
+        @if(session('success'))
+            toastr.success("{{ session('success') }}");
+        @endif
+
+        @if(session('error'))
+            toastr.error("{{ session('error') }}");
+        @endif
+
+        @if(session('warning'))
+            toastr.warning("{{ session('warning') }}");
+        @endif
+
+        @if(session('info'))
+            toastr.info("{{ session('info') }}");
+        @endif
+      </script>
 
 
       
